53f98abf1ae9b5ab1539741aff246208831d1e6c
Saksham Gakhar <44347589+sakshamg94@users.noreply.github.com>
2020-08-26 00:54:24 -0700

    added day-night median disparity func
    if night time median "field" is > day time median value, then tag that night starting time as an anomaly (1 anomaly per 24 hr period)

--- Data Cleaning/anomaly_detector.py
+++ Data Cleaning/anomaly_detector.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 import holoviews as hv
+import datetime
 hv.extension('bokeh')
 
 
@@ -136,9 +137,119 @@ def rollingStdevDev(fields, df, window, min_period, numStd):
     
     return anomaly_dfs, overlays
 
-def anomalyDetector(fields, df, anomaly_types=['negativeFields', 'rollingMedianDev'], 
+def nightDayDisparity(fields, df, min_rec, day_start, day_end):
+    """
+    Function to filter anomalous records for nights where the median 
+    nighttime field value > median daytime field value for the preceding day
+    (1 anomaly per 24 hr period)
+    
+    args:
+    fields:  list containing the string name(s) of the field(s) of interest for anomaly detection  
+    Note these can only bete AQ fields
+    df: pandas dataframe containing 'device', fields, and 'when_captured'
+    min_rec: minimum nuymber of readings for there to be a defined (non-Nan) daytime or nightime median field value
+    day_start: start hour of the day (like 6 for 0600hrs)
+    day_end: end hour for the day (like 18 for 1800hrs)
+    Note: daytime on a date is defined as the hours between `day_start` and `day_end`
+    nighttime on a date is defined as the hours between `day_end` of the current_date and 
+    the `day_start` of the next_date (if next date records are available in the records)
+    
+    return:
+    pandas dataframe containing anomalous records: 4 fields are in the list
+            ['anomaly_type','device','normalized_severity_score','when_captured']
+    overlays: holoviews plot objects with  data segregated by day, night and anomalous positions that need attention
+    """
+    anomaly_dfs = pd.DataFrame()
+    overlays = []
+    gf = df.groupby('device')
+    # add the hour of day column to the data frame
+    df['hod'] = pd.to_datetime(df['when_captured']).dt.hour
+    df['date'] = pd.to_datetime(df['when_captured']).dt.date
+    
+    for device in tqdm(df['device'].unique(), mininterval = 30):
+        deviceFilter = df['device'] == device
+        overlay_batch = []
+        df_dev = gf.get_group(device)
+        sf = df_dev.groupby('date')
+        dates = np.unique(df_dev['date'])
+        # ensure dates are all in increasing order
+        assert(np.all(np.diff(dates).astype('timedelta64[h]').astype('float')) >=0) 
+        
+        for field in fields:
+            # dictionary of [day_median, night_median]for the "field" on that date (with date as the key)
+            medians_sf = {}     
+            # day and night medians are close together, differ by 1-2 or sometimes 0.
+            # report dates and devices as anomalies when the date's night time median is larger than day time median 
+            # -- works for PMS only -- not for radiation
+            for curr_date in dates:
+                # get the median for daytime value for the current date
+                day_mask = (sf.get_group(curr_date).hod >=day_start) & (sf.get_group(curr_date).hod < day_end)
+                day_vals= sf.get_group(curr_date)[field][day_mask]
+                if len(day_vals) >= min_rec:
+                    day_median = day_vals.median()
+                else:
+                    day_median = np.nan
+
+                # get the median nighttime value for the current date
+                night_mask_curr = sf.get_group(curr_date).hod >= day_end
+                night_vals_curr = sf.get_group(curr_date)[field][night_mask_curr]
+
+                night_vals_next = []
+                if curr_date + datetime.timedelta(1) in dates:
+                    night_mask_next = sf.get_group(curr_date + datetime.timedelta(1)).hod < day_start
+                    night_vals_next = sf.get_group(curr_date+ datetime.timedelta(1))[field][night_mask_next]
+
+                night_vals = night_vals_curr.append(night_vals_next)
+                if len(night_vals) >= min_rec:
+                    night_median = night_vals.median()
+                else:
+                    night_median = np.nan
+
+                medians_sf[curr_date] = [day_median, night_median]
+            
+            # create a temporary dictionary to store the "start" timestamps 
+            # for night datapoints where night median>day's median
+            temp= {}
+            temp['when_captured']  = []
+            temp['normalized_severity_score'] = []
+            for date, lst in medians_sf.items():
+                if lst[1]>lst[0]: # nighttime PM > daytime PM
+                    temp['when_captured'].append(sf.get_group(date).iloc[0].when_captured) 
+                    # the the first record to direct user to anomalous night
+                    temp['normalized_severity_score'].append((lst[1]-lst[0] + 1)/(lst[0]+1))
+            temp['anomaly_type'] = np.repeat('nighttime median ' + field + ' > daytime median', len(temp['when_captured']))
+            temp['device'] = np.repeat(device, len(temp['when_captured']))
+            
+            #convert dictionary to dataframe
+            temp_df = pd.DataFrame.from_dict(temp)
+            
+            # visualize
+            night_filter = (df_dev["hod"] >day_end) | (df_dev["hod"] <day_start)
+            morng_filter = ~(night_filter)
+
+            night = hv.Scatter((pd.to_datetime(df_dev['when_captured'][night_filter]).values,\
+                                              df_dev[field][night_filter]), label='night').opts(width=800)
+            morng = hv.Scatter((pd.to_datetime(df_dev['when_captured'][morng_filter]).values,\
+                                                       df_dev[field][morng_filter]), label='day').opts(width=800)
+
+            limit = max(df_dev[field][morng_filter]) # only for illustration purposes -- constant 
+            x_anom = pd.to_datetime(temp_df['when_captured']).values
+            anoms = hv.Points((x_anom, np.repeat(limit, len(x_anom))), label='anomalies').opts(width=800, color='black')
+
+            overlay = morng*night*anoms
+            overlay.opts(title="device: "+str(device))
+            
+            # return the list of anomalies 
+            anomaly_dfs = anomaly_dfs.append(temp_df, ignore_index=True)
+            overlay_batch.append(overlay)
+        overlays.append(overlay_batch)
+    
+    return anomaly_dfs, overlays
+
+def anomalyDetector(fields, df, anomaly_types=['negativeFields', 'rollingMedianDev', 'rollingStdevDev'], 
                     rmd_window=None, rmd_min_period=None, rmd_numStd=None,
-                    rsd_window=None, rsd_min_period=None, rsd_numStd=None):
+                    rsd_window=None, rsd_min_period=None, rsd_numStd=None, 
+                    ndd_min_rec = 5, ndd_day_start = 6, ndd_day_end = 18):
     """
     Wrapper function to run checks for specific types of anomalies and aggregate the results/plots
     
@@ -149,11 +260,14 @@ def anomalyDetector(fields, df, anomaly_types=['negativeFields', 'rollingMedianD
     rmd_window: moving window size for the rolling Median and stddev for rollingMedianDev
     rmd_min_period: Minimum number of observations in window required to have a value (otherwise result is NA) for rollingMedianDev
     rmd_numStd: tolerance in number of standard deviations away from Median for record to be anomalous for rollingMedianDev
+    ndd_min_rec: minimum nuymber of readings for there to be a defined (non-Nan) daytime or nightime median field value
+    ndd_day_start: start hour of the day (like 6 for 0600hrs)
+    ndd_day_end: end hour for the day (like 18 for 1800hrs)
     
     return:
     pandas dataframe containing anomalous records: 4 fields are in the list
             ['anomaly_type','device','normalized_severity_score','when_captured']
-    overlays: dict of holoviews plot objects with running Median, +/- numStd lines, and data
+    overlays: dict of holoviews plot objects 
     """
     anomaly_dfs = []
     overlays = {}
@@ -168,6 +282,10 @@ def anomalyDetector(fields, df, anomaly_types=['negativeFields', 'rollingMedianD
         anomaly_dfs_rsd, overlays_rsd = rollingStdevDev(fields, df, rsd_window, rsd_min_period, rsd_numStd)
         anomaly_dfs.append(anomaly_dfs_rsd)
         overlays['rollingStdevDev'] = overlays_rsd
+    if 'nightDayDisparity' in anomaly_types:
+        anomaly_dfs_ndd, overlays_ndd = nightDayDisparity(fields, df, ndd_min_rec, ndd_day_start, ndd_day_end)
+        anomaly_dfs.append(anomaly_dfs_ndd)
+        overlays['nightDayDisparity'] = overlays_ndd
     if len(anomaly_dfs) > 1:
         anomaly_dfs = pd.concat(anomaly_dfs, axis = 0)
         
